f07d969fd1c5fad70891410f6cacf64a9cd0c2b0
Moritz Mistol <moritz.mistol@gmail.com>
2023-12-16 18:21:59 +0100

    update scripts

--- home/.chezmoiscripts/darwin/run_once_before_00-install-prerequisits.sh
+++ home/.chezmoiscripts/darwin/run_once_before_00-install-prerequisits.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+git config --global user.name "Moritz Mistol"
+git config --global user.email "moritz.mistol@gmail.com"
+git config --global github.user "MoritzM00"
+
 xcode-select --install
+
+# install homebrew if it does not exist
+if command -v brew &> /dev/null; then
+    echo "Homebrew is already installed."
+else
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
