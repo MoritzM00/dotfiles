3fbc8e695851fc4a6dc2843460bd7750026337c0
Moritz Mistol <moritz.mistol@gmail.com>
2023-12-17 14:41:13 +0100

    remove powerlevel10k and oh my zsh install

--- home/.chezmoiscripts/darwin/run_once_before_00-install-prerequisits.sh
+++ home/.chezmoiscripts/darwin/run_once_before_00-install-prerequisits.sh
@@ -9,22 +9,6 @@ else
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-# if oh my zsh is not installed, install it
-if [ -d "$HOME/.oh-my-zsh" ] 
-then
-    echo "oh-my-zsh is already installed."
-else
-    echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
 
-# if powerlevel10k is not installed, install it
-if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ] 
-then
-    echo "powerlevel10k is already installed."
-else
-    echo "Installing powerlevel10k..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-fi
 
 
