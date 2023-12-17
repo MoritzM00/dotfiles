0011814ec4d33ff0f6dedcb5fbcc28ed1fb8fe50
Moritz Mistol <moritz.mistol@gmail.com>
2023-12-17 10:36:37 +0100

    update macos defaults

--- home/.chezmoiscripts/darwin/run_onchange_after_macos-defaults.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_macos-defaults.sh
@@ -3,14 +3,28 @@
 set -eufo pipefail
 
 # https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
 
+###############################################################################
+# General                                                                     
+###############################################################################
 defaults write NSGlobalDomain AppleInterfaceStyle Dark
-defaults write NSGlobalDomain AppleLanguages -array en-US
-defaults write NSGlobalDomain AppleLocale en_US
+defaults write NSGlobalDomain AppleLanguages -array "en" "de"
+defaults write NSGlobalDomain AppleLocale de_DE
 defaults write NSGlobalDomain InitialKeyRepeat -int 25
 defaults write NSGlobalDomain KeyRepeat -int 6
+defaults write -g ApplePressAndHoldEnabled -bool false
+# Disable "natural" scroll
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
-# dock settings
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+###############################################################################
+# Dock                                                                      
+###############################################################################
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
@@ -18,10 +32,57 @@ defaults write com.apple.dock launchanim -bool false
 defaults write com.apple.dock orientation left
 defaults write com.apple.dock show-recents -bool false
 
-defaults write -g ApplePressAndHoldEnabled -bool false
+# Set the icon size of Dock items to 52 pixels
+defaults write com.apple.dock tilesize -int 52
 
-# Disable "natural" scroll
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Hot corners
+
+# Bottom left right screen corner → Desktop
+defaults write com.apple.dock wvous-bl-corner -int 4
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Bottom right screen corner → Start screen saver
+defaults write com.apple.dock wvous-br-corner -int 5
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+
+###############################################################################
+# Finder                                                                      
+###############################################################################
+
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
 
 # Show filename extensions by default
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool tru
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+
+###############################################################################
+# Iterm2                                                                      
+###############################################################################
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
