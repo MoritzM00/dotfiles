193ea0ef2f8827f985ead7e902267bf9779e86b7
Moritz Mistol <moritz.mistol@gmail.com>
2023-12-24 11:25:23 +0100

    install copilot

--- home/dot_config/nvim/lua/custom/plugins/init.lua
+++ home/dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		event = "InsertEnter",
+		opts = {}
+	}
+}
