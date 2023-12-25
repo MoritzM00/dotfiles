38748e910dd9a4f506121c3a4e61cdfe5c573ed8
Moritz Mistol <moritz.mistol@gmail.com>
2023-12-25 10:48:51 +0100

    fix install script

--- home/.chezmoiscripts/darwin/run_once_before_00-install-prerequisits.sh
+++ home/.chezmoiscripts/darwin/run_once_before_00-install-prerequisits.sh
@@ -7,6 +7,8 @@ if command -v brew &> /dev/null; then
 else
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo; echo 'eval' $(/opt/homebrew/bin/brew shellenv) >> /Users/$(whoami)/.zprofile
+    eval $(/opt/homebrew/bin/brew shellenv)
 fi
 
 if command -v dcli &> /dev/null; then
@@ -14,7 +16,8 @@ if command -v dcli &> /dev/null; then
 else
     # Install Dashlane CLI
     echo "Install and setup of Dashlane CLI"
-    brew install dashlane/tap/dashlane-cli
+    brew tap dashlane/tap
+    brew install dashlane-cli
     dcli sync
 fi
 
@@ -24,6 +27,7 @@ if [ -f "/Library/Keyboard Layouts/EurKEY.keylayout" ]; then
     echo "EurKEY keyboard layout is already installed."
 else
     echo "Downloading EurKEY keyboard layout. (Requires restart by user)"
+    brew install wget
     wget https://github.com/jonasdiemer/EurKEY-Mac/raw/master/EurKEY.icns
     wget https://github.com/jonasdiemer/EurKEY-Mac/raw/master/EurKEY.keylayout
     sudo mv EurKEY.icns EurKEY.keylayout "/Library/Keyboard Layouts/"
